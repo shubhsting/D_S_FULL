e807549225a314ec5c44f401bd986bde1917b36b
Shubham <shubhamsingh1840@gmail.com>
2020-10-19 16:10:50 +0530

    updated

--- searching.java
+++ searching.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 public class searching {
     public static void main(String[] args) {
-
+        int[] arr = { 2, 1, 5, 4, 3 };
+        insertion_sort(arr);
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
     }
 
     public static boolean linear_search(int[] arr, int ele) {
@@ -35,5 +38,40 @@ public class searching {
 
         }
         return false;
+    }
+
+    // sorting
+    // selection sort
+
+    public static void selection_sort(int[] arr) {
+        // window ka size window ki value se last tak hai
+        for (int window = 0; window < arr.length; window++) {
+            int windomin = Integer.MAX_VALUE;
+            int minidx = 0;
+            for (int idx = window; idx < arr.length; idx++) {
+                if (arr[idx] < windomin) {
+                    windomin = arr[idx];
+                    minidx = idx;
+                }
+            }
+            // swap
+            arr[minidx] = arr[window];
+            arr[window] = windomin;
+        }
+    }
+
+    public static void insertion_sort(int[] arr) {
+        // window ka size 0 se window ki value tak hai
+        for (int window = 0; window < arr.length; window++) {
+            for (int idx = window - 1; idx >= 0; idx--) {
+                if (arr[idx] > arr[window]) {
+                    // swap
+                    int temp = arr[idx];
+                    arr[idx] = arr[window];
+                    arr[window] = temp;
+                } else
+                    break;
+            }
+        }
     }
 }
